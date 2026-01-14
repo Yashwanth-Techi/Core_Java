482d0b9f72b39885c947a99e51b897a283cd37ba
Yashwanth G M <yashwanthgm692@gmail.com>
2026-01-15 00:12:09 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,48 @@
+//
+//public class Day1 {
+//	public static void main(String[]args) {
+//		
+////		for(int i=0;i<=5;i++) {
+////			for(int j=0;j<=5;j++) {
+////				System.out.print("*");
+////			}
+////			System.out.println();
+////		}
+//		
+//		for(int i=1;i<=3;i++) {
+//			for(int j=1;j<=3;j++) {
+//				System.out.print("1");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+//Triangle Star Pattern
+//Right Triangle Star Pattern
+
+//public class Day1{
+//	public static void main(String[] args) {
+//		for(int i=0;i<=5;i++) {
+//			for(int j=0;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//	}
+//}
+
+//Inverted Right Triangle
+
+//public class Day1{
+//	public static void main(String[]args) {
+//		for(int i=0;i<=5;i++) {
+//			for(int j=0;j<=5;j--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
