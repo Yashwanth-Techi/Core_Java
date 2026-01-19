6d09eff26cbe3329db27d47f363ab03cff80db76
Yashwanth G M <yashwanthgm692@gmail.com>
2026-01-19 23:42:30 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -89,3 +89,38 @@
 //         }
 //     }
 // }
+
+// Hollow Square
+
+// class HollowSquare {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (i == 1 || i == n || j == 1 || j == n)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// Pyramid Pattern
+
+// class Pyramid {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for (int i = 1; i <= n; i++) {
+//             for (int s = 1; s <= n - i; s++) {
+//                 System.out.print(" ");
+//             }
+//             for (int j = 1; j <= 2 * i - 1; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
